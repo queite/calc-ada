c98c4155efefc7f8b92a15aeb04a33dd5f686913
Queite <queitesc@gmail.com>
2024-09-04 21:55:40 -0300

    feat: add script

--- calc.py
+++ calc.py
@@ -15,3 +15,42 @@ def divisao(num1, num2):
         return num1 / num2
     else:
         return "Não é possível realizar divisão por zero."
+
+
+def calculadora():
+    while True:
+        print("Escolha a operação:")
+        print("1. Adição")
+        print("2. Subtração")
+        print("3. Multiplicação")
+        print("4. Divisão")
+        print("5. Sair")
+
+        escolha = input("Digite o número da operação desejada (1/2/3/4/5): ")
+
+        if escolha == '5':
+            print("Encerrando a calculadora. Até mais!")
+            break
+
+        if escolha in ('1', '2', '3', '4'):
+            num1 = float(input("Digite o primeiro número: "))
+            num2 = float(input("Digite o segundo número: "))
+
+            if escolha == '1':
+                resultado = soma(num1, num2)
+            elif escolha == '2':
+                resultado = subtracao(num1, num2)
+            elif escolha == '3':
+                resultado = multiplicacao(num1, num2)
+            elif escolha == '4':
+                resultado = divisao(num1, num2)
+            print("\n****************************")
+            print(f"Resultado: {resultado:.2f}")
+            print("****************************\n")
+        else:
+            print("\n*******************************************************")
+            print("Opção inválida. Por favor, escolha uma operação válida.")
+            print("*******************************************************\n")
+
+
+calculadora()
